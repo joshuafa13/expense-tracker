38b18685242943a46b52a6665863eb5a6f0c8760
Josh Fang <joshuafang13@gmail.com>
2020-08-06 23:12:50 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -73,6 +73,13 @@ app.post('/records/:id/edit', (req, res) => {
 		.catch(error => console.log(error))
 })
 
+app.post('/records/:id/delete', (req, res) => {
+	const id = req.params.id
+	return Record.findById(id)
+		.then(record => record.remove())
+		.then(() => res.redirect('/'))
+		.catch(error => console.log(error))
+})
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`)
 })
