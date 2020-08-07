d6b2e558ed0971b34c70c47134a95f2c3466ae97
Josh Fang <joshuafang13@gmail.com>
2020-08-08 00:14:56 +0800

    fix: fix seeder bug

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -3,14 +3,21 @@ const db = require('../../config/mongoose')
 
 const categories = ['household', 'transportation', 'entertainment', 'food', 'others']
 
+const icons = [
+	'<i class="fas fa-home"></i>',
+	'<i class="fas fa-shuttle-van"></i>',
+	'<i class="fas fa-grin-beam"></i>',
+	'<i class="fas fa-utensils"></i>',
+	'<i class="fas fa-pen"></i>',
+]
+
 db.once('open', () => {
 	console.log('mongodb connected')
 	for (let i = 0; i < categories.length; i++) {
-		Category.create({ name: `${categories[i]}` })
-			.then(() => {
-				db.close()
-				console.log('done')
-			})
-			.catch(error => console.log(error))
+		Category.create({
+			name: `${categories[i]}`,
+			icon: `${icons[i]}`,
+		})
 	}
+	console.log('done')
 })
